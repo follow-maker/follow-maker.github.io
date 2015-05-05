7ef9cc8a5f02729a9e31060dd99843dcacbf86df
Mayank Jain <mayank.jain@tradus.com>
2015-05-05 23:00:51 +0530

    Resynced pages from Unbouncepages

--- sync_pages.sh
+++ sync_pages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rm -f instagram-follow-maker.html twitter-follow-maker.html
+rm -f instagram-follow-maker.html twitter-follow-maker.html tumblr-follow-maker.html
+wget -c --verbose http://unbouncepages.com/tumblr-follow-maker -O tumblr-follow-maker.html
 wget -c --verbose http://unbouncepages.com/instagram-follow-maker -O instagram-follow-maker.html
 wget -c --verbose http://unbouncepages.com/twitter-follow-maker -O twitter-follow-maker.html
 
