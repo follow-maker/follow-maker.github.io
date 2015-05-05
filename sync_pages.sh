ebc4f08a2a017c558bd688358cb704209e7ca8ea
Mayank Jain <mayank.jain@tradus.com>
2015-05-05 20:33:07 +0530

    Resynced pages from Unbouncepages

---
+++ sync_pages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -f instagram-follow-maker.html twitter-follow-maker.html
+wget -c --verbose http://unbouncepages.com/instagram-follow-maker -O instagram-follow-maker.html
+wget -c --verbose http://unbouncepages.com/twitter-follow-maker -O twitter-follow-maker.html
+
+git add .
+git commit -am "Resynced pages from Unbouncepages"
+git push origin master
